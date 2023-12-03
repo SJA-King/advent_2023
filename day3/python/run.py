d323f688a2561e518b3ad89a7ca41c92aaaf5e57
SJA-King <simonjaking@gmail.com>
2023-12-03 21:01:28 +0000

    add day3 working
    Realised i was looking at \n which meant i added parts i didnt want to. Also if a number is at the end i wasnt calculating it which ruined my score

--- day3/python/run.py
+++ day3/python/run.py
@@ -3,87 +3,81 @@ import os
 
 HERE = os.path.dirname(os.path.abspath(__file__))
 EXAMPLE = os.path.join(HERE, "../example.txt")
-EXAMPLE2 = os.path.join(HERE, "../example2.txt")
 INPUT = os.path.join(HERE, "../input.txt")
 
 
 def get_data(the_file: str):
+    data = []
     with open(the_file, 'r') as it:
-        return it.readlines()
+        for line in it.readlines():
+            data.append(line.strip())
+
+    return data
 
 
 def part_1(the_data):
     print("Part 1")
 
-    sum_of_parts = 0
+    parts = []
 
     for line_idx, line in enumerate(the_data):
-        line = line.strip()
         print(f"Line is {line}")
         a_number = ""
         indexes = []
+        # if the number is '.....111' we lose it!
         for col_idx, i_char in enumerate(line):
             if i_char.isnumeric():
                 a_number += i_char
                 indexes.append(col_idx)
-                # print(i_char)
             else:
                 if not a_number:
                     continue
 
-                # print(f"A Number = {a_number}")
                 found_part = False
                 col_first = indexes[0] - 1
                 col_last = indexes[-1] + 1
+                col = 0
+                row = 0
+                char_at_idx = 0
 
-                # check the column before number
-                for i_row in [line_idx - 1, line_idx, line_idx + 1]:
-                    try:
-                        char_at_idx = the_data[i_row][col_first]
-                        if not char_at_idx.isnumeric() and char_at_idx != ".":
-                            # print(f"Found Special Char {char_at_idx}")
-                            found_part = True
-                    except IndexError:
-                        pass
+                # check the column before and after number
+                for i_col in [col_first, col_last]:
+                    for i_row in [line_idx - 1, line_idx, line_idx + 1]:
+                        try:
+                            char_at_idx = the_data[i_row][i_col]
+                            if not char_at_idx.isnumeric() and char_at_idx != ".":
+                                row = i_row
+                                col = i_col
+                                found_part = True
+                                break
+                        except IndexError:
+                            pass
 
-                # check the row above number
-                for i_col in range(col_first, col_last):
-                    try:
-                        char_at_idx = the_data[line_idx - 1][i_col]
-                        if not char_at_idx.isnumeric() and char_at_idx != ".":
-                            # print(f"Found Special Char {char_at_idx}")
-                            found_part = True
-                    except IndexError:
-                        pass
-
-                # check row below number
-                for i_col in range(col_first, col_last):
-                    try:
-                        char_at_idx = the_data[line_idx + 1][i_col]
-                        if not char_at_idx.isnumeric() and char_at_idx != ".":
-                            # print(f"Found Special Char {char_at_idx}")
-                            found_part = True
-                    except IndexError:
-                        pass
-
-                # check coloumn after number
-                for i_row in [line_idx - 1, line_idx, line_idx + 1]:
-                    try:
-                        char_at_idx = the_data[i_row][col_last]
-                        if not char_at_idx.isnumeric() and char_at_idx != ".":
-                            # print(f"Found Special Char {char_at_idx}")
-                            found_part = True
-                    except IndexError:
-                        pass
+                if not found_part:
+                    for i_row in [-1, 1]:
+                        # check the rows above and below number
+                        for i_col in range(col_first, col_last):
+                            try:
+                                char_at_idx = the_data[line_idx + i_row][i_col]
+                                if not char_at_idx.isnumeric() and char_at_idx != ".":
+                                    row = line_idx + i_row
+                                    col = i_col
+                                    found_part = True
+                                    break
+                            except IndexError:
+                                pass
 
                 if found_part:
-                    print(f"A part = {a_number}")
-                    sum_of_parts += int(a_number)
+                    print(f"A part = {a_number}, with Special: '{char_at_idx}' at ({row},{col})")
+                    parts.append(int(a_number))
 
                 a_number = ""
                 indexes = []
 
-    return sum_of_parts
+    print(f"All Parts = {parts}")
+    return sum(parts)
+    # 540374 is too high apparently
+    # 537832 is my actual answer
 
 
 
@@ -97,9 +91,9 @@ def part_2(the_data):
 def main():
     print("Day 3")
 
-    example_data = get_data(EXAMPLE)
-    result = part_1(example_data)
-    print(result)
+    # example_data = get_data(EXAMPLE)
+    # result = part_1(example_data)
+    # print(result)
     # #
     input_data = get_data(INPUT)
     result = part_1(input_data)
--- day3/python/test_run.py
+++ day3/python/test_run.py
@@ -1,24 +1,149 @@
 from run import part_1, part_2
+import random
+import pytest
 
 
-def test_part_1():
-    example_data = [
-        "1abc2",
-        "pqr3stu8vwx",
-        "a1b2c3d4e5f",
-        "treb7uchet"]
-
-    assert 142 == part_1(example_data)
+def replace_str_index(text, index=0, replacement=''):
+    return f'{text[:index]}{replacement}{text[index + 1:]}'
 
 
-def test_part_2():
-    example_data = [
-        "two1nine",
-        "eightwothree",
-        "abcone2threexyz",
-        "xtwone3four",
-        "4nineeightseven2",
-        "zoneight234",
-        "7pqrstsixteen"]
+@pytest.mark.parametrize('index', [0, 1, 2, 3, 4])
+def test_part_1_above(index: int):
+    random_number = random.randint(100, 999)
+    test_string = '.....'
+    test_string = replace_str_index(test_string, index, '+')
 
-    assert 281 == part_2(example_data)
+    test_data = [
+        test_string,
+        f".{random_number}.",
+        "....."]
+
+    assert random_number == part_1(test_data)
+
+
+@pytest.mark.parametrize('index', [0, 1, 2, 3])
+def test_part_1_above_two_digit(index: int):
+    random_number = random.randint(10, 99)
+    test_string = '.....'
+    test_string = replace_str_index(test_string, index, '+')
+
+    test_data = [
+        test_string,
+        f".{random_number}..",
+        "....."]
+
+    assert random_number == part_1(test_data)
+
+
+@pytest.mark.parametrize('index', [1, 2, 3])
+def test_part_1_above_one_digit(index: int):
+    random_number = random.randint(1, 9)
+    test_string = '.....'
+    test_string = replace_str_index(test_string, index, '+')
+
+    test_data = [
+        test_string,
+        f"..{random_number}..",
+        "....."]
+
+    assert random_number == part_1(test_data)
+
+
+def test_part_1_aside():
+    random_number = random.randint(100, 999)
+    test_data = [
+        '.....',
+        f'-{random_number}.',
+        '.....']
+
+    assert random_number == part_1(test_data)
+
+    test_data = [
+        '.....',
+        f'.{random_number}-',
+        '.....']
+
+    assert random_number == part_1(test_data)
+
+
+@pytest.mark.parametrize('index', [0, 1, 2, 3, 4])
+def test_part_1_below(index: int):
+    random_number = random.randint(100, 999)
+    test_string = '.....'
+    test_string = replace_str_index(test_string, index, '%')
+
+    test_data = [
+        '.....',
+        f'.{random_number}.',
+        test_string]
+
+    assert random_number == part_1(test_data)
+
+
+@pytest.mark.parametrize('index', [0, 1, 2, 3])
+def test_part_1_below_two_digit(index: int):
+    random_number = random.randint(10, 99)
+    test_string = '.....'
+    test_string = replace_str_index(test_string, index, '+')
+
+    test_data = [
+        ".....",
+        f".{random_number}..",
+        test_string]
+
+    assert random_number == part_1(test_data)
+
+
+@pytest.mark.parametrize('index', [1, 2, 3])
+def test_part_1_below_one_digit(index: int):
+    random_number = random.randint(1, 9)
+    test_string = '.....'
+    test_string = replace_str_index(test_string, index, '+')
+
+    test_data = [
+        ".....",
+        f"..{random_number}..",
+        test_string]
+
+    assert random_number == part_1(test_data)
+
+
+@pytest.mark.parametrize('a_char', ['*', '@', '+', '-', '/', '=', '%', '$', '&'])
+def test_part_1_all_special_chars_above(a_char: str):
+    random_number = random.randint(100, 999)
+    random_col = random.randint(0, 4)
+    test_string = replace_str_index('.....', random_col, a_char)
+    test_data = [
+        test_string,
+        f'.{random_number}.',
+        '.....']
+
+    assert random_number == part_1(test_data)
+
+
+@pytest.mark.parametrize('a_char', ['*', '@', '+', '-', '/', '=', '%', '$', '&'])
+def test_part_1_all_special_chars_below(a_char: str):
+    random_number = random.randint(100, 999)
+    random_col = random.randint(0, 4)
+    test_string = replace_str_index('.....', random_col, a_char)
+    test_data = [
+        '.....',
+        f'.{random_number}.',
+        test_string]
+
+    assert random_number == part_1(test_data)
+
+
+def test_part_1_example():
+    example_data = ["467..114..",
+                    "...*......",
+                    "..35..633.",
+                    "......#...",
+                    "617*......",
+                    ".....+.58.",
+                    "..592.....",
+                    "......755.",
+                    "...$.*....",
+                    "664.598.."]
+
+    assert 4361 == part_1(example_data)
